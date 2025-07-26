cae2eb756939e9861a939025bd41b67d5bc109a9
Lenovo <panneylal005@gmail.com>
2025-07-26 17:58:39 +0530

    tetsting commit

--- RapperClassDemo.java
+++
@@ -1,9 +0,0 @@
-public class RapperClassDemo {
-    public static void main(String args[])
-    {
-     int a=30;
-     Integer i=Integer.valueOf(a);
-     Integer j=a;
-     System.out.println(a+""+i+""+j);
-    }
-}
